8b908d91e3fd9b8ad038f9bf7014c607af70858d
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-07-29 19:21:22 +0200

    dl is ahrd

--- start_db.sh
+++ start_db.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
-cd ./dynamodb_local_latest
-java -D"java.library.path=./DynamoDBLocal_lib" -jar DynamoDBLocal.jar -sharedDb
+
+ENDPOINT="https://s3.eu-central-1.amazonaws.com/dynamodb-local-frankfurt"
+DIR="./dynamodb_local_latest"
+TARBALL="$DIR.tar.gz"
+
+if [[ ! -d $DIR ]]; then
+  mkdir -p "$DIR/third_party_licenses" "$DIR/DynamoDBLocal_lib"
+  curl "$ENDPOINT/$TARBALL" | tar zxf -C=$DIR - 
+fi
+
+java -D"java.library.path=$DIR/DynamoDBLocal_lib" -jar "$DIR/DynamoDBLocal.jar" -sharedDb
+
+sleep 5
