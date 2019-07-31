6b1ff9b8618365d3580c89a4154356dab75acd21
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-07-31 08:37:30 +0200

    fix db start script

--- start_db.sh
+++ start_db.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-ENDPOINT="https://s3.eu-central-1.amazonaws.com/dynamodb-local-frankfurt"
+URL="https://s3.eu-central-1.amazonaws.com/dynamodb-local-frankfurt/dynamodb_local_latest.tar.gz"
 DIR="./dynamodb_local_latest"
-TARBALL="$DIR.tar.gz"
 
 if [[ ! -d $DIR ]]; then
   mkdir $DIR
-  curl --progress-bar "$ENDPOINT/$TARBALL" | tar --directory=$DIR -zxf - 
+  curl --progress-bar $URL | tar --directory=$DIR -zxf - 
 fi
 
-java -D"java.library.path=$DIR/DynamoDBLocal_lib" -jar "$DIR/DynamoDBLocal.jar" -sharedDb
+cd $DIR
+
+java -D"java.library.path=DynamoDBLocal_lib" -jar "DynamoDBLocal.jar" -sharedDb
