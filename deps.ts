e4b48b401d30e56cf27bad3f0af2b8cc6dd6f831
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-12-22 14:09:49 +0100

    impl: lock deps to latest versions

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export { encode, decode } from "https://denopkg.com/chiefbiiko/std-encoding/mod.ts";
-export { hmac } from "https://denopkg.com/chiefbiiko/hmac/mod.ts";
+export { encode, decode } from "https://denopkg.com/chiefbiiko/std-encoding@v1.0.0/mod.ts";
+export { hmac } from "https://denopkg.com/chiefbiiko/hmac@v1.0.1/mod.ts";
 export {
   toUint8Array as base64ToUint8Array,
   fromUint8Array as base64FromUint8Array
 } from "https://deno.land/x/base64@v0.2.0/mod.ts";
-export { sha256 } from "https://denopkg.com/chiefbiiko/sha256/mod.ts";
+export { sha256 } from "https://denopkg.com/chiefbiiko/sha256@v1.0.0/mod.ts";
